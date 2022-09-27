400e384ded11ed5e18e4785028feb73ac843d13a
unrealJune <jphilip@umd.edu>
2022-09-27 10:23:41 -0400

    Implement feature get-valid-moves

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -14,7 +14,20 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
+    ArrayList<Location> valid_moves = new ArrayList<Location>();
+    if(this.myLoc.x - 1 >= 0){
+      valid_moves.add(new Location(this.myLoc.x - 1, this.myLoc.y));
+    }
+    if(this.myLoc.y - 1 >= 0){
+      valid_moves.add(new Location(this.myLoc.x, this.myLoc.y - 1));
+    }
+    valid_moves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+    valid_moves.add(new Location(this.myLoc.x , this.myLoc.y + 1));
+    valid_moves.add(new Location(this.myLoc.x + 1, this.myLoc.y+1));
+
+    valid_moves.removeIf(loc -> this.myMap.getLoc(loc).contains(Map.Type.WALL));
+
+    return valid_moves;
   }
 
   public boolean move() {
