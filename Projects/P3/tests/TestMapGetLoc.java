955c9684471730385bf9508a76951106d08ef003
unrealJune <jphilip@umd.edu>
2022-09-29 12:28:44 -0400

    Update TestMapGetLoc.java

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -1,9 +1,22 @@
-import java.io.*;
-import junit.framework.*;
+import junit.framework.TestCase;
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
 
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
-    return null;
+//Creating A Map
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens
+    // Initialized
+
+//Creating Players
+    Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red);
+    PacMan pacman = frame.addPacMan(new Location(3, 3));
+    assert(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.GHOST));
+    assert(frame.getMap().getLoc(new Location(3, 3)).contains(Map.Type.PACMAN));
+    assert(frame.getMap().getLoc(new Location(60, 60)).contains(Map.Type.EMPTY));
+    PacMan pacman2 = frame.addPacMan(new Location(1, 1));
+    assert(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
   }
 }
