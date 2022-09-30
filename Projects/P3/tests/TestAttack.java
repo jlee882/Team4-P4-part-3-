9c2055f4ab9bbcab3002f9a53724f9907725c0b4
Jahanzeb Hassan <jahanzebhassan1234@gmail.com>
2022-09-30 17:32:47 -0400

    test

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -1,9 +1,19 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
 public class TestAttack extends TestCase {
 
   public void testAttack() throws FileNotFoundException {
-    return null;
+	  NoFrame frame = new NoFrame();	    
+	  PacMan pacman = frame.addPacMan(new Location(1, 1));
+	  Ghost ghostInRange = frame.addGhost(new Location(1, 2), "GHOST", Color.RED);
+	  Ghost ghostOutOfRange = frame.addGhost(new Location(1, 6), "GHOST", Color.GREEN);
+	  Map map = frame.getMap();
+	  	   
+	  assert !ghostOutOfRange.attack();
+	  
+	  assert ghostInRange.attack();
+	  assert map.isGameOver();
   }
 }
