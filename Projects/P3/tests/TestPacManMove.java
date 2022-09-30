a1f748282e61974b3aa808205a9a4cb5d19e2165
Jahanzeb Hassan <jahanzebhassan1234@gmail.com>
2022-09-30 17:19:48 -0400

    added test

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -1,9 +1,17 @@
 import java.io.*;
+import java.util.List;
+
 import junit.framework.*;
 
 public class TestPacManMove extends TestCase {
 
-  public void testPacManMove() throws FileNotFoundException {
-    return null;
+public void testPacManMove() throws FileNotFoundException {
+	  NoFrame frame = new NoFrame();	    
+	  PacMan pacman = frame.addPacMan(new Location(1, 1));
+	  List<Location> locations = pacman.get_valid_moves();
+	  
+	  pacman.move();
+	  
+	  assert locations.contains(pacman.myLoc);
   }
 }
