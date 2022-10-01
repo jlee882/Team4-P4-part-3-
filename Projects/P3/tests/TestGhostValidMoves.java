c971dfa433461daf2f4a45081917b3a5616c3f84
achen6159 <annie.sharon.chen@gmail.com>
2022-10-01 18:20:22 -0400

    made test for ghost-valid_moves()

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -4,6 +4,16 @@ import junit.framework.*;
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
-    return null;
+    NoFrame frame = new NoFrame(); 
+
+    // only can go down or to the right
+    Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); 
+    ArrayList<Location> possible_locs = new ArrayList<Location>();
+    Location down = new Location(1, 2);
+    Location right = new Location(2, 1);
+
+    possible_locs.append(down, right);
+
+    assertTrue(ghost.get_valid_moves().containsAll(possible_locs));
   }
 }
