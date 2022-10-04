6f7e2dbff3b4352263622f2c7a1cbd24994a77b6
achen6159 <annie.sharon.chen@gmail.com>
2022-10-03 23:34:24 -0400

    updated test file

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -3,15 +3,13 @@ import junit.framework.*;
 
 public class TestMapEatCookie extends TestCase {
 
-  @Test
   public void testMapEatCookie() throws FileNotFoundException {
-    Mainframe frame = new MainFrame(); 
     NoFrame frame = new NoFrame(); 
 
     PacMan pacman = frame.addPacMan(new Location(1, 2));
-    assertTrue(frame.getMap.eatCookie("pacman") != null);
+    assert (frame.getMap().eatCookie("pacman") != null);
 
     PacMan pacman2 = frame.addPacMan(new Location(0, 0));
-    assertTrue(frame.getMap.eatCookie("pacman") == null);
+    assert (frame.getMap().eatCookie("pacman") == null);
   }
 }
